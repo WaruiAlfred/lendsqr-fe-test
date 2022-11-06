3b643c9fa22704b463df4ce50ef9aef2778a11e7
Alfred Warui <alfredwarui17@gmail.com>
2022-11-07 01:33:33 +0300

    install and configure material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.scss";
+import { Button } from "@mui/material";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button variant="contained">Welcome</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
