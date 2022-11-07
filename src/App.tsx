7065a69da94c8d7c1e1973e1c70adade806fa90a
Alfred Warui <alfredwarui17@gmail.com>
2022-11-08 00:05:46 +0300

    cleanup initial code

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,7 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.scss";
-import { Button } from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button variant="contained">Welcome</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <div className="App">My app</div>;
+};
 
 export default App;
