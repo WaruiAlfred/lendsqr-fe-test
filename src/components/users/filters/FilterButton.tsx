d24f4defffe633c532093b4336fd8eb158825c68
Alfred Warui <alfredwarui17@gmail.com>
2022-11-10 17:58:46 +0300

    add functionality for opening filter form to btn

--- src/components/users/filters/FilterButton.tsx
+++ src/components/users/filters/FilterButton.tsx
@@ -1,13 +1,49 @@
-import { IconButton, Tooltip } from "@mui/material";
+import { Fragment, useState } from "react";
+import { Box, IconButton, Menu, MenuItem, Tooltip } from "@mui/material";
 import FilterListIcon from "@mui/icons-material/FilterList";
+import FilterForm from "./FilterForm";
 
 const FilterButton = () => {
+  const [anchorFilterEl, setAnchorFilterEl] = useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenFilterForm = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorFilterEl(event.currentTarget);
+  };
+
+  const handleCloseFilterForm = () => {
+    setAnchorFilterEl(null);
+  };
+
   return (
-    <Tooltip title="Filter list">
-      <IconButton>
-        <FilterListIcon />
-      </IconButton>
-    </Tooltip>
+    <Fragment>
+      <Tooltip title="Filter list">
+        <IconButton onClick={handleOpenFilterForm} sx={{ p: 0 }}>
+          <FilterListIcon />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        sx={{ mt: "45px" }}
+        id="menu-appbar"
+        anchorEl={anchorFilterEl}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        open={Boolean(anchorFilterEl)}
+        onClose={handleCloseFilterForm}
+      >
+        <MenuItem onClick={handleCloseFilterForm}>
+          <FilterForm />
+        </MenuItem>
+      </Menu>
+    </Fragment>
   );
 };
 
