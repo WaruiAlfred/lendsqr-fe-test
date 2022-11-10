a5f26a37a59f200b12fa806433579825319d403b
Alfred Warui <alfredwarui17@gmail.com>
2022-11-10 17:52:41 +0300

    create TableOutline component

---
+++ src/components/user/TableOutline.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+import Table from "@mui/material/Table";
+import TableContainer from "@mui/material/TableContainer";
+import Paper from "@mui/material/Paper";
+
+const TableOutline: FC<{ children: any }> = ({ children }) => {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="user data table">
+        {children}
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default TableOutline;
