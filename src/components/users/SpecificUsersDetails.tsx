a117752b0887ec164f3847d7bbb409575c7b3b6b
Alfred Warui <alfredwarui17@gmail.com>
2022-11-11 01:56:43 +0300

    display all users data

--- src/components/users/SpecificUsersDetails.tsx
+++ src/components/users/SpecificUsersDetails.tsx
@@ -21,6 +21,9 @@ import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import PersonRemoveAlt1OutlinedIcon from "@mui/icons-material/PersonRemoveAlt1Outlined";
 import HowToRegOutlinedIcon from "@mui/icons-material/HowToRegOutlined";
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { fetchAllUsers } from "../../api";
+import moment from "moment";
 
 interface Column {
   id: "organization" | "username" | "email" | "phone" | "date" | "status";
@@ -83,44 +86,44 @@ function createData(
   return { organization, username, email, phone, date, status };
 }
 
-const rows = [
-  createData(
-    "Lendsqr",
-    "Adedeji",
-    "adedeji@lendsqr.com",
-    "0712345678",
-    "May 15,2020 10:00 AM",
-    "Inactive"
-  ),
-  createData(
-    "Lendsqr",
-    "Adedeji",
-    "adedeji@lendsqr.com",
-    "0712345678",
-    "May 15,2020 10:00 AM",
-    "Inactive"
-  ),
-  createData(
-    "Lendsqr",
-    "Adedeji",
-    "adedeji@lendsqr.com",
-    "0712345678",
-    "May 15,2020 10:00 AM",
-    "Inactive"
-  ),
-  // createData("United States", "US", 327167434, 9833520),
-  // createData("Canada", "CA", 37602103, 9984670),
-  // createData("Australia", "AU", 25475400, 7692024),
-  // createData("Germany", "DE", 83019200, 357578),
-  // createData("Ireland", "IE", 4857000, 70273),
-  // createData("Mexico", "MX", 126577691, 1972550),
-  // createData("Japan", "JP", 126317000, 377973),
-  // createData("France", "FR", 67022000, 640679),
-  // createData("United Kingdom", "GB", 67545757, 242495),
-  // createData("Russia", "RU", 146793744, 17098246),
-  // createData("Nigeria", "NG", 200962417, 923768),
-  // createData("Brazil", "BR", 210147125, 8515767),
-];
+// const rows = [
+//   createData(
+//     "Lendsqr",
+//     "Adedeji",
+//     "adedeji@lendsqr.com",
+//     "0712345678",
+//     "May 15,2020 10:00 AM",
+//     "Inactive"
+//   ),
+//   // createData(
+//   //   "Lendsqr",
+//   //   "Adedeji",
+//   //   "adedeji@lendsqr.com",
+//   //   "0712345678",
+//   //   "May 15,2020 10:00 AM",
+//   //   "Inactive"
+//   // ),
+//   // createData(
+//   //   "Lendsqr",
+//   //   "Adedeji",
+//   //   "adedeji@lendsqr.com",
+//   //   "0712345678",
+//   //   "May 15,2020 10:00 AM",
+//   //   "Inactive"
+//   // ),
+//   // createData("United States", "US", 327167434, 9833520),
+//   // createData("Canada", "CA", 37602103, 9984670),
+//   // createData("Australia", "AU", 25475400, 7692024),
+//   // createData("Germany", "DE", 83019200, 357578),
+//   // createData("Ireland", "IE", 4857000, 70273),
+//   // createData("Mexico", "MX", 126577691, 1972550),
+//   // createData("Japan", "JP", 126317000, 377973),
+//   // createData("France", "FR", 67022000, 640679),
+//   // createData("United Kingdom", "GB", 67545757, 242495),
+//   // createData("Russia", "RU", 146793744, 17098246),
+//   // createData("Nigeria", "NG", 200962417, 923768),
+//   // createData("Brazil", "BR", 210147125, 8515767),
+// ];
 
 const options = [
   { id: "O1", text: "View Details", icon: <VisibilityOutlinedIcon /> },
@@ -133,6 +136,22 @@ const SpecificUsersDetails = () => {
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
     null
+  );
+  const { data } = useQuery({ queryKey: ["users"], queryFn: fetchAllUsers });
+  console.log(data);
+
+  const rows = data?.map((dataItem: any) =>
+    createData(
+      dataItem.orgName,
+      dataItem.userName,
+      dataItem.email,
+      dataItem.profile.phoneNumber,
+      moment(dataItem.createdAt).format("MMM D, YYYY h:mm A"),
+      +moment(dataItem.lastActiveDate)?.fromNow()?.slice(0) <= 7 &&
+        moment(dataItem.lastActiveDate).fromNow().includes("day")
+        ? "Active"
+        : "Inactive"
+    )
   );
 
   const handleChangePage = (event: unknown, newPage: number) => {
@@ -174,8 +193,8 @@ const SpecificUsersDetails = () => {
           </TableHead>
           <TableBody>
             {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
+              ?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row: any) => {
                 return (
                   <TableRow hover role="checkbox" tabIndex={-1} key={row.email}>
                     {columns.map((column) => {
@@ -242,7 +261,7 @@ const SpecificUsersDetails = () => {
       <TablePagination
         rowsPerPageOptions={[10, 25, 100]}
         component="div"
-        count={rows.length}
+        count={rows?.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
